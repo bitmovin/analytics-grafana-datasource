8d220a45a294db0d5f679a610a08fb4298e45cd7
Germano Fronza <germano.inf@gmail.com>
2018-10-09 13:10:26 -0300

    Added a new interval value: AUTO

--- src/types/intervals.js
+++ src/types/intervals.js
@@ -2,7 +2,19 @@ export const QUERY_INTERVAL = {
   MINUTE: 'MINUTE',
   HOUR: 'HOUR',
   DAY: 'DAY',
-  MONTH: 'MONTH'
+  MONTH: 'MONTH',
+  AUTO: 'AUTO'
 };
 export const QUERY_INTERVAL_LIST = Object.keys(QUERY_INTERVAL).map(key => QUERY_INTERVAL[key]);
 
+export const calculateAutoInterval = (intervalMs) => {
+  if (intervalMs < 60000) {
+    return QUERY_INTERVAL.MINUTE;
+  } else if (intervalMs >= 60000 && intervalMs < 604800) {
+    return QUERY_INTERVAL.HOUR;
+  } else if (intervalMs >= 604800 && intervalMs < 2592000) {
+    return QUERY_INTERVAL.DAY;
+  } else {
+    return QUERY_INTERVAL.MONTH;
+  }
+}
