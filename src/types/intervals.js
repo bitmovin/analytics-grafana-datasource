6aea650881db01fa03c92f3531c91f6faa8aac95
Peter Pfaffeneder <peter.pfaffeneder@gmail.com>
2020-05-27 12:21:41 +0200

    Added helper functions to pad time series

--- src/types/intervals.js
+++ src/types/intervals.js
@@ -21,3 +21,25 @@ export const calculateAutoInterval = (intervalMs) => {
     return QUERY_INTERVAL.MONTH;
   }
 }
+
+/**
+ * Get corresponding interval in milliseconds.
+ * @param {String} interval The interval
+ * @returns {number} Interval in milliseconds or -1 if unknown.
+ */
+export const intervalToMilliseconds = (interval) => {
+  switch(interval) {
+    case QUERY_INTERVAL.SECOND:
+      return 1000;
+    case QUERY_INTERVAL.MINUTE:
+      return 1000*60;
+    case QUERY_INTERVAL.HOUR:
+      return 1000*60*60;
+    case QUERY_INTERVAL.DAY:
+      return 1000*60*60*24;
+    case QUERY_INTERVAL.MONTH:
+      return 1000*60*60*24*30;
+    default:
+      return -1;
+  }
+};
