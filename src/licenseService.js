fc9047bab74ceee60b36eecfef92d2d4ba0d3adc
Peter Pfaffeneder <peter.pfaffenedet@bitmovin.com>
2022-02-23 13:35:17 +0100

    Enable selection of virtual licenses

--- src/licenseService.js
+++ src/licenseService.js
@@ -1,30 +1,59 @@
 class LicenseService {
+
   constructor(requestHandler, baseURL) {
     this.requestHandler = requestHandler;
     this.baseURL = baseURL;
   }
 
-  async fetchLicenses() {
+  async _fetchLicensesForEndpoint(endpoint, mapperFunction) {
     const requestOptions = {
-      url: this.baseURL + '/analytics/licenses',
+      url: this.baseURL + endpoint,
       method: 'GET',
     };
-    try {
-      const licensesResponse = await this.requestHandler.doRequest(requestOptions);
 
-      if (licensesResponse.status !== 200) {
-        return [];
-      }
+    const licensesResponse = await this.requestHandler.doRequest(requestOptions);
 
-      const licenses = [];
-      for (var item of licensesResponse.data.data.result.items) {
-        item['label'] = item.name ? item.name : item.licenseKey;
-        licenses.push(item);
-      }
-      return licenses;
-    } catch (e) {
+    if (licensesResponse.status !== 200) {
       return [];
     }
+
+    const licenses = [];
+    for (const item of licensesResponse.data.data.result.items) {
+      const license = mapperFunction(item);
+      licenses.push(license);
+    }
+    return licenses;
+  }
+
+  async fetchLicenses() {
+    let allLicenses = [];
+
+    const licenseEndpoints = [
+      {
+        endpoint: '/analytics/licenses',
+        mapperFunc: (item) => ({
+          ...item,
+          'label': item.name ? item.name : item.licenseKey
+        }),
+      },
+      {
+        endpoint: '/analytics/virtual-licenses',
+        mapperFunc: (item) => ({
+          ...item,
+          'licenseKey': item.id,
+          'label': item.name ? item.name : item.id
+        }),
+      },
+    ];
+    for (const item of licenseEndpoints) {
+      try {
+        const licenses = await this._fetchLicensesForEndpoint(item.endpoint, item.mapperFunc);
+        allLicenses = allLicenses.concat(licenses);
+      } catch (e) {
+        // ignore error
+      }
+    }
+    return allLicenses;
   }
 };
 
