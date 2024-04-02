dea6369d3818605ab719c7d2c603a09a425ceb7b
MGJamJam <myriam.gantner@gmail.com>
2024-04-02 15:34:53 -0300

    implements the testDatasource method

--- bitmovin-analytics-datasource/src/datasource.ts
+++ bitmovin-analytics-datasource/src/datasource.ts
@@ -6,10 +6,13 @@ import {
   MutableDataFrame,
   FieldType,
 } from '@grafana/data';
+import { getBackendSrv } from '@grafana/runtime';
+import { lastValueFrom } from 'rxjs';
 
 import { MyQuery, MyDataSourceOptions } from './types';
 
 export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
+  baseUrl: string;
   apiKey: string;
   tenantOrgId?: string;
   adAnalytics?: boolean;
@@ -20,6 +23,7 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     this.apiKey = instanceSettings.jsonData.apiKey;
     this.tenantOrgId = instanceSettings.jsonData.tenantOrgId;
     this.adAnalytics = instanceSettings.jsonData.adAnalytics;
+    this.baseUrl = instanceSettings.url!;
   }
 
   async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
@@ -41,11 +45,32 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     return { data };
   }
 
-  async testDatasource() {
-    // Implement a health check for your data source.
-    return {
-      status: 'success',
-      message: 'Success',
+  async request(url: string, method: string) {
+    const options = {
+      url: this.baseUrl + url,
+      headers: { 'X-Api-Key': this.apiKey },
+      method: method,
     };
+    const response = getBackendSrv().fetch(options);
+
+    return lastValueFrom(response);
+  }
+
+  async testDatasource() {
+    const response = await this.request('/analytics/licenses', 'GET');
+
+    if (response.status === 200) {
+      return {
+        status: 'success',
+        message: 'Datasource setup successfully.',
+      };
+    } else {
+      return {
+        status: 'error',
+        message: response.statusText
+          ? response.statusText
+          : 'An unexpected error occurred. Datasource not setup correctly.',
+      };
+    }
   }
 }
