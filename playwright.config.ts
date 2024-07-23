558abcc62bb2477ad72141e4a302fd0a6e82a656
MGJamJam <myriam.gantner@gmail.com>
2024-07-23 15:23:41 -0300

    deletes commented code in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,52 +52,5 @@ export default defineConfig<PluginOptions>({
       },
       dependencies: ['auth'],
     },
-    // {
-    //   name: 'run-tests Firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //     // @grafana/plugin-e2e writes the auth state to this file,
-    //     // the path should not be modified
-    //     storageState: 'playwright/.auth/admin.json',
-    //   },
-    //   dependencies: ['auth'],
-    // },
-    // {
-    //   name: 'run-tests Safari',
-    //   use: {
-    //     ...devices['Desktop Safari'],
-    //     // @grafana/plugin-e2e writes the auth state to this file,
-    //     // the path should not be modified
-    //     storageState: 'playwright/.auth/admin.json',
-    //   },
-    //   dependencies: ['auth'],
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
